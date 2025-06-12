dc7a78477858c5f836bab818c9f7aabd6976950f
Brandon H. Goding <brandon.h.goding@gmail.com>
2025-06-12 19:34:37 -0400

    feat(header): added header to page

---
+++ www/src/components/header/header.js
@@ -0,0 +1,11 @@
+import './header.scss';
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="header-content">
+                <h1>Fort Kent Cinema</h1>
+            </div>
+        </header>
+    );
+};
+export default Header;
