908661d2d1562f8168156cad6b2a27429de75a78
Brandon Goding <bgoding@hometap.com>
2025-06-13 08:00:58 -0400

    feat(now_playing): now playing section

--- www/src/App.js
+++ www/src/App.js
@@ -1,13 +1,31 @@
 import './App.css';
 import Header from "./components/header/header";
 import Footer from "./components/footer/footer";
+import NowPlaying from "./components/now_playing/now_playing";
+
+const movies = [
+  {
+    title: "Karate Kid: Legends",
+    rating: "PG-13",
+    length: "1h 34m",
+    youtubeId: "LhRXf-yEQqA",
+    imdbUrl: "https://www.imdb.com/title/tt1674782/"
+  },
+  {
+    title: "How to Train Your Dragon",
+    rating: "PG",
+    length: "2h 5m",
+    youtubeId: "22w7z_lT6YM",
+    imdbUrl: "https://www.imdb.com/title/tt26743210/"
+  }
+];
 
 function App() {
   return (
     <div className="App">
       <Header />
         <div className="Body">
-
+            <NowPlaying movies={movies} />
         </div>
       <Footer />
     </div>
